d37d7ed3d62ae9be169f57aedae6b54c719b6d94
Greg Kane <gbkane@gmail.com>
2018-09-12 11:08:11 -0400

    enforce custom domain when finding vendor

--- app/controllers/spree/cust/static_content_controller.rb
+++ app/controllers/spree/cust/static_content_controller.rb
@@ -3,14 +3,13 @@ module Spree
     class StaticContentController < Spree::Cust::CustomerHomeController
       rescue_from ActiveRecord::RecordNotFound, with: :render_404
       before_action :ensure_user_has_accounts
+      before_action :load_vendor, only: :show
 
       helper 'spree/products'
       layout :determine_layout
 
       def show
         @customer = current_customer
-        @vendors = @customer.vendors.order('name ASC')
-        @vendor = current_vendor
         if @vendor.present?
           @page = @vendor.pages.visible.find_by_slug!(request.path)
           @pages_footer = @vendor.pages.footer_links
@@ -22,6 +21,15 @@ module Spree
       end
 
       private
+
+      def load_vendor
+        if request.host == ENV['DEFAULT_URL_HOST']
+          flash[:error] = 'We were unable to find the page you requested.'
+          redirect_to root_path
+        else
+          @vendor = current_customer.vendors.find_by_custom_domain(request.host)
+        end
+      end
 
       def determine_layout
         return @page.layout if @page && @page.layout.present? && !@page.render_layout_as_partial?
